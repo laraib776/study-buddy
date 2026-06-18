bc7d60ad6c9d8d5c8a04577097767d6958c7c714
Laraib Khalid <120477982+laraib776@users.noreply.github.com>
2026-06-18 23:25:43 +0500

    port Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,7 @@ import app from './app.js';
 dotenv.config();
 
 const PORT = Number(process.env.PORT || 8787);
-
+// const PORT = Number(process.env.PORT || 5000);
 
 const server = app.listen(PORT, () => {
   console.log(`StudyBuddy backend listening on http://localhost:${PORT}`);
